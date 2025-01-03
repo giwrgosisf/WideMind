775b4c6ab0b411a2c3cfffad58a0bc43bb0290f2
giwrgosisf <164655527+giwrgosisf@users.noreply.github.com>
2025-01-03 03:18:19 +0200

    Part2 Ex1 index and category

---
+++ Part2 - BackEnd/public/scripts/category.js
@@ -0,0 +1,84 @@
+
+const url = "https://learning-hub-1whk.onrender.com";
+
+document.addEventListener("DOMContentLoaded", () => {
+    initCategory();
+});
+
+
+async function initCategory() {
+    let myHeaders = new Headers();
+    myHeaders.append('Accept', 'application/json');
+
+    let init = {
+        method: "GET",
+        headers: myHeaders
+    }
+    try {
+
+        const urlParams = new URLSearchParams(window.location.search);
+        const categoryId = urlParams.get('id');
+
+        if (!categoryId) {
+            document.getElementById("subcat-main").innerHTML =
+                "<p>Error: No category ID provided in the URL.</p>";
+            return;
+        }
+
+
+        const fetchedCategories = await fetchData(url + "/categories");
+        // console.log("Fetched Categories:", fetchedCategories);
+        const category = fetchedCategories.find((obj) => obj.id == categoryId);
+
+        const items = await fetchData(url + "/learning-items?category=" + categoryId, init);
+        const books = items.filter(item => item.type === "Book");
+        const lectures = items.filter(item => item.type === "Lecture");
+
+
+        const templateSource = document.getElementById("category-template").textContent;
+        const template = Handlebars.compile(templateSource);
+
+
+        const htmlData = {
+            books: books,
+            lectures: lectures,
+        };
+
+
+        const contentOfTemplate = template(htmlData);
+        document.getElementById("subcat-main").innerHTML = contentOfTemplate;
+
+
+
+        const templateSourceIntro = document.getElementById("category-title-template").textContent;
+        const templateIntro = Handlebars.compile(templateSourceIntro);
+
+
+        const htmlDataIntro = {
+            categoryTitle: category.title,
+        };
+
+
+        const contentOfTemplateIntro = templateIntro(htmlDataIntro);
+        document.getElementById("upper-page").innerHTML = contentOfTemplateIntro;
+
+
+
+    } catch(error) {
+        console.error("Error loading category page:", error);
+        document.getElementById("upper-page").innerHTML =
+          "<p>Failed to load the items for this category. Please try again later.</p>";
+    }
+
+}
+
+
+
+
+async function fetchData(url, init) {
+    const response = await fetch(url, init);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+}
---
+++ Part2 - BackEnd/public/scripts/homePage.js
@@ -0,0 +1,58 @@
+const url = "https://learning-hub-1whk.onrender.com";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    initIndex();
+});
+
+
+async function initIndex() {
+
+    let myHeaders = new Headers();
+    myHeaders.append('Accept', 'application/json');
+
+    let init = {
+        method: "GET",
+        headers: myHeaders
+    }
+
+
+    try {
+        const categories = await fetchData(url + "/categories", init);
+
+        const categoriesWithSubcategories = await Promise.all(
+            categories.map(async (category) => {
+                const subcategories = await fetchData(url + "/categories/" + category.id + "/subcategories", init);
+
+                return {
+                    ...category,
+                    subcategories: subcategories,
+                };
+            })
+        );
+
+        const templateSource = document.getElementById("index-template").textContent;
+
+        const template = Handlebars.compile(templateSource);
+
+        const contentOfTemplate = template({ categories: categoriesWithSubcategories });
+
+        document.getElementById("categories").innerHTML = contentOfTemplate;
+
+
+    } catch (error) {
+        console.error("Error fetching categories or subcategories:", error);
+        document.getElementById("categories").innerHTML  = "<p>Failed to load categories. Please try again later.</p>";
+    }
+
+}
+
+async function fetchData(url, init) {
+    const response = await fetch(url, init);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+}
+
+
